eee8f4a19abbc7edc4341c27a9954c8327323414
xm9512 <14836267@qq.com>
2023-06-07 17:48:57 +0800

    debug all

--- lanuch_server.sh
+++ lanuch_server.sh
@@ -2,10 +2,11 @@ echo ". $HOME/miniconda/etc/profile.d/conda.sh" >> $HOME/.bashrc
 source $HOME/.bashrc >> /dev/null
 conda init bash
 
-conda activate gr
-pip install numpy==1.24.3
-pip install pandas==2.0.1
-pip install gradio==3.30.0
-pip install Pillow==9.4.0
+conda activate test
+export PYTHONPATH=/home/xlab-app-center/miniconda/envs/test/lib/python3.8/site-packages
+# pip install numpy==1.24.3
+# pip install pandas==2.0.1
+# pip install gradio==3.30.0
+# pip install Pillow==9.4.0
 
 python server.py
