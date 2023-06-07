961615b86a503d44b610fa379468e7aa6d489061
Abel <14836267@qq.com>
2023-06-07 15:45:17 +0800

    Update app.py
    adjust conda

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import os
 
 # subprocess.call(["bash", "install_conda.sh"])
 # subprocess.call(["bash", "install_env.sh"])
-subprocess.call(["bash", "lanuch_server.sh"])
+subprocess.call(["bash", "source activate codiff && python server.py"])
