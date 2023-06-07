b0506d9fb894dd8250abb35cc48cf09549e7707f
Abel <14836267@qq.com>
2023-06-07 15:29:41 +0800

    Update app.py
    test os.system

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import subprocess
+import os
 
 # subprocess.call(["bash", "install_conda.sh"])
 # subprocess.call(["bash", "install_env.sh"])
-subprocess.call(["bash", "conda activate codiff && python $HOME/server.py"])
+# subprocess.call(["bash", "conda activate codiff && python $HOME/server.py"])
+os.system("conda activate codiff && python $HOME/server.py")
