376c1abd5248efc8d9a82055e23548b45ceb7d89
xm9512 <14836267@qq.com>
2023-06-07 17:57:47 +0800

    test

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import subprocess
 
 # subprocess.call(["bash", "install_conda.sh"])
 # subprocess.call(["bash", "install_env.sh"])
-# subprocess.call(["bash", "lanuch_server.sh"])
+subprocess.call(["bash", "lanuch_server.sh"])
 ################################################
 
-import gradio as gr
+# import gradio as gr
 
 
-def greet(name):
-    return "Hello " + name + "!"
+# def greet(name):
+#     return "Hello " + name + "!"
 
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+# demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 
-if __name__ == "__main__":
-    demo.launch()
+# if __name__ == "__main__":
+#     demo.launch()
